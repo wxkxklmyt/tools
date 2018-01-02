cf7d466cbcb35d2fe97582b2c0da7bc65a9c3521
我的梦 <519468341@qq.com>
2018-01-02 10:12:45 +0800

    增加生成唯一订单号（格式：YYYY-MMDD-HHII-SS-NNNN,NNNN-CC）

--- src/Tools.php
+++ src/Tools.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * 工具箱（https://www.4ui.cn）
+ * 常用功能，不想重复造轮子
+ */
 namespace wxkxklmyt;
 
 class Tools{
@@ -24,6 +28,7 @@ class Tools{
 
     /**
      * 浏览器console控制台输出
+     * 
      * @param string $str 内容
      */
     public static function console_log($str = ''){
@@ -36,5 +41,29 @@ class Tools{
         }
         
         echo "<script>console.log('$str');</script>";
+    }
+
+    /**
+     * 生成24位唯一订单号码（格式：YYYY-MMDD-HHII-SS-NNNN,NNNN-CC，其中：YYYY=年份，MM=月份，DD=日期，HH=24格式小时，II=分，SS=秒，NNNNNNNN=随机数，CC=检查码）
+     */
+    public static function build_order_id(){
+        // 订购日期
+        $order_date = date('Y-m-d');
+        
+        // 订单号码主体（YYYYMMDDHHIISSNNNNNNNN）
+        $order_id_main = date('YmdHis') . rand(10000000, 99999999);
+        
+        // 订单号码主体长度
+        $order_id_len = strlen($order_id_main);
+        $order_id_sum = 0;
+        
+        for($i = 0; $i < $order_id_len; $i++){
+            $order_id_sum += (int)(substr($order_id_main, $i, 1));
+        }
+        
+        // 唯一订单号码（YYYYMMDDHHIISSNNNNNNNNCC）
+        $order_id = $order_id_main . str_pad((100 - $order_id_sum % 100) % 100, 2, '0', STR_PAD_LEFT);
+        
+        return $order_id;
     }
 }
